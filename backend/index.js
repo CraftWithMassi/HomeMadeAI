5af011668e8f5ef5efff5bce21b6244e9fbdae85
unknown <cwmbusinesses@gmail.com>
2025-11-26 23:05:22 +0100

    tired with this buggggg

--- backend/index.js
+++ backend/index.js
@@ -51,4 +51,7 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-app.listen(3001, () => console.log("Backend running on port 3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
